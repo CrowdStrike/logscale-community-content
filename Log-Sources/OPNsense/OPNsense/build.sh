a9fa67926e612801b22c5faf89a98facb583ab40
Andrew <andrew.latham@crowdstrike.com>
2023-01-25 22:02:50 +1100

    Initial Commit

---
+++ Log-Sources/OPNsense/OPNsense/build.sh
@@ -0,0 +1,25 @@
+set -e
+
+VERSION=$(grep version src/manifest.yaml | grep -o "\d*\.\d*\.\d*")
+
+# Figure out the archive name
+ARCHIVE=logscale-community-content--opnsense-opnsense--${VERSION}.zip
+
+# Delete anything that might already be here
+rm -f Packages/$ARCHIVE
+
+# Create a new zip archive
+(
+    cd src
+    zip -r ../Packages/$ARCHIVE \
+	manifest.yaml \
+	actions \
+	alerts \
+	queries \
+	parsers \
+	dashboards \
+	scheduled-searches \
+	README.md
+)
+
+echo Created package $ARCHIVE
